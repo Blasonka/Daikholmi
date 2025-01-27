a4a6dcaaf7bf11a76e111e0885b009650ae78af3
Blasek Balázs <blasek.balazs@edu.bme.hu>
2025-01-27 12:36:50 +0100

    nav & footer

---
+++ web/diakholmi/resources/views/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+<!-- Footer -->
+<div class="py-3 my-0 text-center bg-body-secondary">
+    <ul class="nav justify-content-center border-bottom pb-2 mb-0">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Nyitólap</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Oktatás</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Jelentkezés</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Cégünkről</a></li>
+    </ul>
+    <ul class="nav justify-content-center">
+        <li class="nav-item"><i class="fas fa-envelope me-3"></i><a href="mailto:diakholmi@gmail.com"
+                class="nav-link px-2 text-muted"><i>diakholmi@gmail.com</i> </a></li>
+        <li class="nav-item"><i class="fas fa-phone me-3"></i><a href="tel:+36209691417"
+                class="nav-link px-2 text-muted"><i>+36 20
+                    969 1417</i> </a></li>
+    </ul>
+    <div class="text-center pb-3">
+        © 2025
+        <a class="text-center text-muted" href="https://diakholmi.hu/">Diákholmi</a>
+    </div>
+</div>
