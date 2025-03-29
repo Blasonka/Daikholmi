b6c0b767270517e036cea10d78654aa117ca3082
Blasonka <blasek.balazs@gmail.com>
2025-03-30 00:08:37 +0100

    db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            // Add charset and collation
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
