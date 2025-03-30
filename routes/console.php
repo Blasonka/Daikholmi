42b542884cea07d847c0258049e6bbe503a7b3b1
Blasonka <blasek.balazs@gmail.com>
2025-03-30 12:19:08 +0200

    sitemap

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Spatie\Sitemap\SitemapGenerator;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('sitemap:generate', function () {
+    SitemapGenerator::create(config('app.url'))
+        ->writeToFile(public_path('sitemap.xml'));
+})->describe('Generate the sitemap.');
